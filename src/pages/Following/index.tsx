d5d1c0c8951eb5ff07f23a3110ed4622415a7c2e
ronalddsilva <ronald@rbmweb.com.br>
2021-09-27 05:56:04 -0300

    first commit

---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { Wrapper, Container, Main } from "./styles";
+
+const Following: React.FC = () => {
+  return (
+    <View>
+      <Text>Following</Text>
+    </View>
+  );
+};
+
+export default Following;
