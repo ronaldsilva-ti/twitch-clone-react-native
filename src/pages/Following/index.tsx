0a6a08d00d05d7fb685700ca93bc67740dba306d
ronalddsilva <ronald@rbmweb.com.br>
2021-09-28 06:58:52 -0300

    feature:criado Header do App e logica de listagem de itens em Following

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,13 +1,88 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+
+import Header from "../../components/Header";
+import Heading from "../../components/Heading";
 
 import { Wrapper, Container, Main } from "./styles";
 
+interface Item {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean;
+}
+
 const Following: React.FC = () => {
+  const { data, indicies } = React.useMemo(() => {
+    const items: Item[] = [
+      {
+        key: "PAGE_HEADING",
+        render: () => <Heading>Following</Heading>,
+      },
+      {
+        key: "FOLLOWED_CATEGORIES",
+        render: () => <View />,
+        isTitle: true,
+      },
+      {
+        key: "C1",
+        render: () => <View />,
+      },
+      {
+        key: "LIVE_CHANNELS",
+        render: () => <View />,
+        isTitle: true,
+      },
+      {
+        key: "C2",
+        render: () => <View />,
+      },
+      {
+        key: "CONTINUE_WATCHING",
+        render: () => <View />,
+        isTitle: true,
+      },
+      {
+        key: "C3",
+        render: () => <View />,
+      },
+      {
+        key: "OFFLINE_CHANNELS",
+        render: () => <View />,
+        isTitle: true,
+      },
+      {
+        key: "C4",
+        render: () => <View />,
+      },
+    ];
+
+    const indicies: number[] = [];
+    items.forEach((item, index) => item.isTitle && indicies.push(index));
+
+    return {
+      data: items,
+      indicies,
+    };
+  }, []);
+
   return (
-    <View>
-      <Text>Following</Text>
-    </View>
+    <Wrapper>
+      <Container>
+        <Header />
+        <Main>
+          <FlatList<Item>
+            data={data}
+            renderItem={({ item }) => item.render()}
+            keyExtractor={(item) => item.key}
+            stickyHeaderIndices={indicies}
+            //Refresh Effects
+            onRefresh={() => {}}
+            refreshing={false}
+          />
+        </Main>
+      </Container>
+    </Wrapper>
   );
 };
 
---
+++ src/pages/Following/styles.ts
@@ -0,0 +1,24 @@
+import { Platform } from "react-native";
+import styled from "styled-components/native";
+import Constants from "expo-constants";
+import colors from "../../styles/colors";
+
+const statusBarHeight =
+  Platform.OS === "android" ? Constants.statusBarHeight : 0;
+
+export const Wrapper = styled.SafeAreaView`
+  background-color: ${colors.primary};
+  flex: 1;
+  padding-top: ${statusBarHeight + "px"};
+`;
+
+export const Heading = styled.Text`
+  color: ${colors.purple};
+  font-family: roboto_500;
+  font-size: 30px;
+`;
+
+export const Container = styled.View`
+  padding-left: 14px;
+`;
+export const Main = styled.View``;
